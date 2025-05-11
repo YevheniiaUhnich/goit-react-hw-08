c5a4f1e3c8470cf1c05dc855205a8d6907fa0a47
YevheniiaUhnich <140949326+YevheniiaUhnich@users.noreply.github.com>
2025-05-11 23:12:18 +0300

    update

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,15 +1,9 @@
 import { Navigate } from "react-router-dom";
 import { selectIsLoggedIn } from "../redux/auth/selectors";
 import { useSelector } from "react-redux";
-import PropTypes from "prop-types";
 
 export const PrivateRoute = ({ element, redirectTo = "/login" }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
   return isLoggedIn ? element : <Navigate to={redirectTo} />;
-};
-
-PrivateRoute.propTypes = {
-  element: PropTypes.element.isRequired,
-  redirectTo: PropTypes.string,
 };
--- src/components/RestrictedRoute.jsx
+++ src/components/RestrictedRoute.jsx
@@ -1,15 +1,9 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { selectIsLoggedIn } from "../redux/auth/selectors";
-import PropTypes from "prop-types";
 
 export const RestrictedRoute = ({ element, redirectTo = "/contacts" }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
   return isLoggedIn ? <Navigate to={redirectTo} /> : element;
-};
-
-RestrictedRoute.propTypes = {
-  element: PropTypes.element.isRequired,
-  redirectTo: PropTypes.string,
 };
